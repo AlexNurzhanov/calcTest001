5259d7f130ccaa52fd07e200bcbd41c9b48ce62d
TurkinAlex <turk.alek@yandex.ru>
2023-05-26 15:38:51 +0400

    первые 5 по кальк

---
+++ app/src/main/java/com/example/calc001/MainActivity.java
@@ -0,0 +1,95 @@
+package com.example.calc001;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    String oldNumder;
+    String operator;
+    Boolean isNew=true;
+    EditText editText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        editText = findViewById(R.id.editText);
+    }
+
+    public void ClickNumber(View view) {
+        if (isNew)
+            editText.setText("");
+        isNew=false;
+        String number = editText.getText().toString();
+        switch (view.getId()){
+            case R.id.but1: number = number+"1" ; break;
+            case R.id.but2: number = number+"2" ; break;
+            case R.id.but3: number = number+"3" ; break;
+            case R.id.but4: number = number+"4" ; break;
+            case R.id.but5: number = number+"5" ; break;
+            case R.id.but6: number = number+"6" ; break;
+            case R.id.but7: number = number+"7" ; break;
+            case R.id.but8: number = number+"8" ; break;
+            case R.id.but9: number = number+"9" ; break;
+            case R.id.but0: number = number+"0" ; break;
+
+            case R.id.butDot:
+                if (dotIsPresent(number)){
+                }else{
+                number = number + ".";
+                }
+                break;
+
+            case R.id.butPlusMinus: number = "-"+number ; break;
+        }
+        editText.setText(number);
+    }
+
+
+    public void operation(View view) {
+        isNew = true;
+        oldNumder = editText.getText().toString();
+        switch (view.getId()){
+            case R.id.butMinus: operator = "-" ; break;
+            case R.id.butPlus: operator = "+" ; break;
+            case R.id.butDivide: operator = "/" ; break;
+            case R.id.butMultiply: operator ="*" ; break;
+        }
+    }
+
+    public void ClickEqual(View view) {
+        String newNumber = editText.getText().toString();
+        Double result = 0.0;
+        switch (operator) {
+            case "-": result = Double.parseDouble(oldNumder) - Double.parseDouble(newNumber); break;
+            case "+": result = Double.parseDouble(oldNumder) + Double.parseDouble(newNumber); break;
+            case "*": result = Double.parseDouble(oldNumder) * Double.parseDouble(newNumber); break;
+            case "/": result = Double.parseDouble(oldNumder) / Double.parseDouble(newNumber); break;
+        }
+        editText.setText(result.toString());
+    }
+
+    public void acClick(View view) {
+        editText.setText("0");
+        isNew = true;
+    }
+
+    public boolean dotIsPresent(String number){
+
+        if (number.indexOf(".")==-1 ){
+            return false;
+        }else {
+            return true;
+        }
+
+
+    }
+
+
+}
